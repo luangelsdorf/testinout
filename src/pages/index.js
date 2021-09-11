6e98b61b7701f35347c33e319cc614f52f6609ff
luangelsdorf <luanferreira2136@gmail.com>
2021-09-11 18:55:36 -0300

    cube

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Home() {
+
+  return (
+    <div className="scene">
+      <div className="shape">
+        <div className="face front">1</div>
+
+        <div className="face back">6</div>
+
+        <div className="face right">4</div>
+
+        <div className="face left">3</div>
+
+        <div className="face top">5</div>
+
+        <div className="face bottom">2</div>
+      </div>
+    </div>
+  )
+}
