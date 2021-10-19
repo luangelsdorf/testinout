017969860065505a73496456e0c56e5168cfe8f6
luangelsdorf <luanferreira2136@gmail.com>
2021-10-19 11:33:23 -0300

    asd

---
+++ src/pages/cube.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Cube from 'src/components/Cube';
+
+export default function CubePage() {
+  return (
+    <>
+      <title>CSS Cube</title>
+      <Cube />
+    </>
+  )
+}
