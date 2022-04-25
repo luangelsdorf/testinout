24cbe4889b86e42e73acb3ac293ca98fcaa658f4
luangelsdorf <luanferreira2136@gmail.com>
2022-04-25 14:52:55 -0300

    Tech&Creativity animation

--- src/pages/stroke-animation.js
+++ src/pages/stroke-animation.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Tech from 'src/components/Tech&Creat';
 import Icon from '../../src/components/Icon';
 
 export default function animationPage() {
   return (
-    <Icon />
+    <Tech />
   )
 }
